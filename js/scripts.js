753db254a884bf155567a7fcadbb529e0c8f22a8
Cesar Lopez <maclopez88@yahoo.com>
2021-11-01 21:17:18 -0700

    add working divide, mult, subtract buttons!
    Co-Authored-by: btapfer@gmail.com
    Co-authored-by: Nick Sullivan <nicholassull@gmail.com>
    Co-authored-by: Amy Young <youngamy1223@gmail.com>
    Co-authored-by: Bryan Tapfer <btapfer@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -20,11 +20,43 @@ function divide(number1, number2) {
 // Everything below this line is user interface logic:
 
 $(document).ready(function() {
-  $("form#add").submit(function(event) {
-    event.preventDefault();
-    const number1 = parseInt($("#add1").val());
-    const number2 = parseInt($("#add2").val());
-    const result = add(number1, number2);
-    $("#output").text(result);
+  $("button#add-button").click(function() {
+    $("form#add").submit(function(event) {
+      event.preventDefault();
+      const number1 = parseInt($("#add1").val());
+      const number2 = parseInt($("#add2").val());
+      const result = add(number1, number2);
+      $("#output").text(result);
+    });
   });
+
+  $("button#subtract-button").click(function() {
+    $("form#add").submit(function(event) {
+      event.preventDefault();
+      const number1 = parseInt($("#add1").val());
+      const number2 = parseInt($("#add2").val());
+      const result = subtract(number1, number2);
+      $("#output").text(result);
+    });
+  });  
+  
+  $("button#multiply-button").click(function() {
+    $("form#add").submit(function(event) {
+      event.preventDefault();
+      const number1 = parseInt($("#add1").val());
+      const number2 = parseInt($("#add2").val());
+      const result = multiply(number1, number2);
+      $("#output").text(result);
+    });
+  });
+
+  $("button#divide-button").click(function() {
+    $("form#add").submit(function(event) {
+      event.preventDefault();
+      const number1 = parseInt($("#add1").val());
+      const number2 = parseInt($("#add2").val());
+      const result = divide(number1, number2);
+      $("#output").text(result);
+    });
+  })
 });
