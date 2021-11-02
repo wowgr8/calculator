fce7dacb721fe6d4478e8cf9aae06066a818db98
Cesar Lopez <maclopez88@yahoo.com>
2021-11-01 20:08:13 -0700

    add lesson updates (business/interface logic) to .js and html

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,30 @@
+// Business logic:
+
 function add(number1, number2) {
   return number1 + number2;
 }
 
-const number1 = parseInt(prompt("Enter a number:"));
-const number2 = parseInt(prompt("Enter another number"));
+function subtract(number1, number2) {
+  return number1 - number2;
+}
 
-alert(add(number1, number2));
+function multiply(number1, number2) {
+  return number1 * number2;
+}
+
+function divide(number1, number2) {
+  return number1 / number2;
+}
+
+
+// Everything below this line is user interface logic:
+
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#add1").val());
+    const number2 = parseInt($("#add2").val());
+    const result = add(number1, number2);
+    $("#output").text(result);
+  });
+});
